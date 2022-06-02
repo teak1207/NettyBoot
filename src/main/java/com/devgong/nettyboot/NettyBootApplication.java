cfccbaf089a007adf83cb1c370980c6135e5edfa
gtgong <gtgong@scsol.co.kr>
2022-06-02 10:49:57 +0900

    feat : 백업

--- src/main/java/com/devgong/nettyboot/NettyBootApplication.java
+++ src/main/java/com/devgong/nettyboot/NettyBootApplication.java
@@ -9,5 +9,6 @@ public class NettyBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(NettyBootApplication.class, args);
     }
+    // main() 메소드가 선언된 클래스로, 스프링부트가 시작되는 시점이다.
 
 }
--- src/main/java/com/devgong/nettyboot/config/ApplicationStartupTask.java
+++ src/main/java/com/devgong/nettyboot/config/ApplicationStartupTask.java
@@ -10,9 +10,10 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ApplicationStartupTask implements ApplicationListener<ApplicationReadyEvent> {
+    //ApplicationReadyEvent : 스프링부트 서비스를 시작 시 초기화하는 코드를 Bean으로 만들 때 사용
+    //여기서는 네티 서버 소켓을 실행하여 incoming connection을 받을 준비를 합니다.
 
     private final NettyServerSocket nettyServerSocket;
-
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
